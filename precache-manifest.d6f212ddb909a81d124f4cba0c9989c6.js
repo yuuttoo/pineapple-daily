49d801bdf59f7830a0309acdcc92c74b35ee2b62
yuuto <yuuttoo@gmail.com>
2019-09-10 13:54:19 +0800

    Updates

---
+++ precache-manifest.d6f212ddb909a81d124f4cba0c9989c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe505be1e23a4b741a46c7e64de5c4f",
+    "url": "/pineapple-daily/index.html"
+  },
+  {
+    "revision": "bf6ea9f0720d1ae86349",
+    "url": "/pineapple-daily/static/css/main.46a01157.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/pineapple-daily/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "bf6ea9f0720d1ae86349",
+    "url": "/pineapple-daily/static/js/main.879fb164.chunk.js"
+  },
+  {
+    "revision": "6096673fbcbd7e23301b",
+    "url": "/pineapple-daily/static/js/runtime~main.56bd353d.js"
+  }
+]);
